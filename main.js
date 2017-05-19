73e3e569d4a51f3728514553cb2b5e0a23b9a44a
Travis Wood <travis_cw@yahoo.com>
2017-05-19 11:19:58 -0700

    Add a route to chat screen

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import Expo from 'expo';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 
 // This is just a simple screen that is passed into the navigator
@@ -10,17 +10,38 @@ class App extends React.Component {
   };
 
   render() {
+    // navigation helpers come in to elements as props
+    const { navigate } = this.props.navigation;
     return (
       <View style={styles.container}>
         <Text>Open up main.js to start working on your app!</Text>
+        <Button
+          onPress={() => navigate('Chat')}
+          title="Chat with Lucy"
+        />
       </View>
     );
   }
 }
 
+class ChatScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Chat with Lucy right now!',
+  };
+  render() {
+    return (
+      <View>
+        <Text>Chat with Lucy</Text>
+      </View>
+    )
+  }
+}
+
 // This navigator is what gets registered by Expo!
 const SimpleApp = StackNavigator({
+  // Each of the screens has to correspond with the actual component
   Home: { screen: App },
+  Chat: { screen: ChatScreen }
 });
 
 const styles = StyleSheet.create({
